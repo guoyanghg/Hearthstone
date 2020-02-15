d162728d87a2a7d1d082731889fceeda9cb49d30
Yang Guo <yangguo@Yangs-MacBook-Pro.local>
2020-02-14 17:18:03 -0800

    render testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var cardService = require('../services/cardService');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  cardService.getCardsByClassName("Rogue")
+      .then(cards => {
+        res.render('index', { cards: cards });
+      });
 });
 
 module.exports = router;
